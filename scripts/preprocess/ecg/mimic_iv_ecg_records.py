1f657971e6ee93ab351650dbc68b54e03ec5cfcc
bill <williamlacava@gmail.com>
2025-01-08 16:30:08 -0500

    formatting changes for mimic-iv 1.0

--- scripts/preprocess/ecg/mimic_iv_ecg_records.py
+++ scripts/preprocess/ecg/mimic_iv_ecg_records.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from preprocess import FEMALE_VALUE, MALE_VALUE
 
+
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -27,8 +28,9 @@ def get_parser():
         required=False,
         help="Path to the MIMIC-IV root directory (optional).",
     )
-    
+
     return parser
+
 
 def process_patients(path: str) -> pd.DataFrame:
     """
@@ -46,21 +48,44 @@ def process_patients(path: str) -> pd.DataFrame:
     """
     patients = pd.read_csv(path)
 
-    patients.rename({'gender': 'sex'}, axis=1, inplace=True)
-    patients["sex"] = patients["sex"].map({"F": FEMALE_VALUE, "M": MALE_VALUE}).astype("Int64")
+    patients.rename({"gender": "sex"}, axis=1, inplace=True)
+    patients["sex"] = (
+        patients["sex"].map({"F": FEMALE_VALUE, "M": MALE_VALUE}).astype("Int64")
+    )
 
-    assert ((patients['anchor_year_group'].str.slice(stop=4).astype(int) + 2) == 
-            patients['anchor_year_group'].str.slice(start=-4).astype(int)).all()
+    assert (
+        (patients["anchor_year_group"].str.slice(stop=4).astype(int) + 2)
+        == patients["anchor_year_group"].str.slice(start=-4).astype(int)
+    ).all()
 
-    patients['anchor_year_group_middle'] = patients['anchor_year_group'].str.slice(stop=4).astype(int) + 1
-    patients['anchor_year_offset'] = patients['anchor_year_group_middle'] - patients['anchor_year']
-    patients['anchor_day_offset'] = (patients['anchor_year_offset'] * 365.25).astype('timedelta64[D]')
-    patients['dod_anchored'] = pd.to_datetime(patients['dod']) + patients['anchor_day_offset']
-    patients.drop(['anchor_year', 'anchor_year_group', 'anchor_year_offset', 'dod'], axis=1, inplace=True)
-    patients.rename({'anchor_year_group_middle': 'anchor_year', 'dod_anchored': 'dod'}, axis=1, inplace=True)
-    patients['anchor_year_dt'] = pd.to_datetime({'year': patients['anchor_year'], 'month': 1, 'day': 1})
+    patients["anchor_year_group_middle"] = (
+        patients["anchor_year_group"].str.slice(stop=4).astype(int) + 1
+    )
+    patients["anchor_year_offset"] = (
+        patients["anchor_year_group_middle"] - patients["anchor_year"]
+    )
+    patients["anchor_day_offset"] = (patients["anchor_year_offset"] * 365.25).astype(
+        "timedelta64[D]"
+    )
+    patients["dod_anchored"] = (
+        pd.to_datetime(patients["dod"]) + patients["anchor_day_offset"]
+    )
+    patients.drop(
+        ["anchor_year", "anchor_year_group", "anchor_year_offset", "dod"],
+        axis=1,
+        inplace=True,
+    )
+    patients.rename(
+        {"anchor_year_group_middle": "anchor_year", "dod_anchored": "dod"},
+        axis=1,
+        inplace=True,
+    )
+    patients["anchor_year_dt"] = pd.to_datetime(
+        {"year": patients["anchor_year"], "month": 1, "day": 1}
+    )
 
     return patients
+
 
 def process_cardiac_markers(path: str) -> pd.DataFrame:
     """
@@ -79,19 +104,24 @@ def process_cardiac_markers(path: str) -> pd.DataFrame:
     cardiac_marker_chunks = []
     for chunk in pd.read_csv(path, chunksize=1e5, low_memory=False):
         filtered_chunk = chunk[
-            chunk['itemid'].isin([
-                # 51002, # Troponin I (troponin-I is not measured in MIMIC-IV)
-                # 52598, # Troponin I, point of care, rare/poor quality
-                51003, # Troponin T
-                50911, # Creatinine Kinase, MB isoenzyme
-            ])
+            chunk["itemid"].isin(
+                [
+                    # 51002, # Troponin I (troponin-I is not measured in MIMIC-IV)
+                    # 52598, # Troponin I, point of care, rare/poor quality
+                    51003,  # Troponin T
+                    50911,  # Creatinine Kinase, MB isoenzyme
+                ]
+            )
         ]
         cardiac_marker_chunks.append(filtered_chunk)
 
     cardiac_markers = pd.concat(cardiac_marker_chunks)
-    cardiac_markers['itemid'].map({51003: 'Troponin T', 50911: 'Creatine Kinase, MB Isoenzyme'})
+    cardiac_markers["itemid"].map(
+        {51003: "Troponin T", 50911: "Creatine Kinase, MB Isoenzyme"}
+    )
 
     return cardiac_markers
+
 
 def main(args):
     """
@@ -103,11 +133,9 @@ def main(args):
         Command line arguments.
     """
     os.makedirs(args.processed_root, exist_ok=True)
-    
-    record_list_csv = os.path.join(
-        args.raw_root,
-        "mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0/record_list.csv",
-    )
+
+    record_list_csv = os.path.join(args.raw_root, "record_list.csv")
+    print(f"reading {record_list_csv}")
     records = pd.read_csv(record_list_csv)
 
     results = {}
@@ -116,38 +144,44 @@ def main(args):
         patients = process_patients(
             os.path.join(args.mimic_iv_root, "hosp/patients.csv.gz")
         )
-        results['patients'] = patients
-        
+        results["patients"] = patients
+
         # Update acquisition times and ages based on anchor group
-        records = records.merge(patients, how='left', on='subject_id')
+        records = records.merge(patients, how="left", on="subject_id")
 
-        records['ecg_time'] = pd.to_datetime(records['ecg_time']) + \
-            records['anchor_day_offset']
+        records["ecg_time"] = (
+            pd.to_datetime(records["ecg_time"]) + records["anchor_day_offset"]
+        )
 
-        records['age'] = records['anchor_age'] + \
-            (records['ecg_time'] - records['anchor_year_dt']).dt.days / 365.25
+        records["age"] = (
+            records["anchor_age"]
+            + (records["ecg_time"] - records["anchor_year_dt"]).dt.days / 365.25
+        )
 
         # Process cardiac markers
         cardiac_markers = process_cardiac_markers(
             os.path.join(args.mimic_iv_root, "hosp/labevents.csv.gz")
         )
-        results['cardiac_markers'] = cardiac_markers
+        results["cardiac_markers"] = cardiac_markers
 
-    # Incorporate machine diagnoses/reports
-    machine_diagnoses = pd.read_csv(
-        os.path.join(args.raw_root, "mimic_iv_ecg_machine_diagnoses.csv")
-    )
-    records['machine_diagnosis'] = machine_diagnoses['0']
+    # WGL: commented this out because these files aren't in MIMIC-IV-ECG 1.0, they are in the older unavailable version
+    # # Incorporate machine diagnoses/reports
+    # machine_diagnoses = pd.read_csv(
+    #     os.path.join(args.raw_root, "mimic_iv_ecg_machine_diagnoses.csv")
+    # )
+    # records['machine_diagnosis'] = machine_diagnoses['0']
 
-    machine_report = pd.read_csv(
-        os.path.join(args.raw_root, "mimic_iv_ecg_machine_report.csv")
-    )
-    records['machine_report'] = machine_report['0']
-    results['records'] = records
+    # machine_report = pd.read_csv(
+    #     os.path.join(args.raw_root, "mimic_iv_ecg_machine_report.csv")
+    # )
+    # records['machine_report'] = machine_report['0']
+    results["records"] = records
 
     # Save results
     for filename, data in results.items():
+        print(f"writing {filename}.csv")
         data.to_csv(os.path.join(args.processed_root, f"{filename}.csv"), index=False)
+
 
 if __name__ == "__main__":
     parser = get_parser()
--- scripts/preprocess/ecg/mimic_iv_ecg_signals.py
+++ scripts/preprocess/ecg/mimic_iv_ecg_signals.py
@@ -23,7 +23,7 @@ def postprocess_mimic_iv_ecg_meta(meta: pd.DataFrame):
 
 def main(args):
     records = pd.read_csv(os.path.join(args.processed_root, "records.csv"))
-    records["path"] = os.path.join(args.raw_root, "mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0") + '/' + records["path"]
+    records["path"] = args.raw_root + '/' + records['path'] 
     records["save_file"] = (SOURCE + "_") + \
         filenames_from_paths(records["path"], replacement_ext=".mat")
     records["source"] = SOURCE
